a14701ffe686a8efeb707dbad7ac1933438a81a4
Szymon Sus <sus.szymon3@gmail.com>
2021-03-12 14:03:50 +0100

    Added Address and Customer repositories

---
+++ src/ServiceQuotes.Infrastructure/Repositories/AddressRepository.cs
@@ -0,0 +1,11 @@
+﻿using ServiceQuotes.Domain.Entities;
+using ServiceQuotes.Domain.Repositories;
+using ServiceQuotes.Infrastructure.Context;
+
+namespace ServiceQuotes.Infrastructure.Repositories
+{
+    public class AddressRepository : Repository<Address>, IAddressRepository
+    {
+        public AddressRepository(AppDbContext dbContext) : base(dbContext) { }
+    }
+}
---
+++ src/ServiceQuotes.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using ServiceQuotes.Domain.Entities;
+using ServiceQuotes.Domain.Repositories;
+using ServiceQuotes.Infrastructure.Context;
+
+namespace ServiceQuotes.Infrastructure.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(AppDbContext dbContext) : base(dbContext) { }
+    }
+}
