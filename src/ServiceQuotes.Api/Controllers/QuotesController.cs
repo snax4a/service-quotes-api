5b6896cb53aaafcdf1c7509d05365cb98a92aea1
SKVetr <sebastian966@gmail.com>
2021-05-23 19:51:05 +0200

    Added missing GetQuoteById to Quote Controller.

--- src/ServiceQuotes.Api/Controllers/QuotesController.cs
+++ src/ServiceQuotes.Api/Controllers/QuotesController.cs
@@ -26,6 +26,20 @@ namespace ServiceQuotes.Api.Controllers
             return Ok(await _quoteService.GetAllQuotes(filter));
         }
 
+        [Authorize]
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(typeof(GetQuoteResponse), 200)]
+        public async Task<ActionResult<GetQuoteResponse>> GetQuoteById(Guid id)
+        {
+            var quote = await _quoteService.GetQuoteById(id);
+
+            if (quote is null) return NotFound();
+
+            return Ok(quote);
+        }
+
         [Authorize(Role.Manager)]
         [HttpPut("{id:guid}/status")]
         [ProducesResponseType(401)]
